7084516d66632d2ae4f646b3b1aef46d62738c1a
Douglas Winter <132670815+DGzzzzz@users.noreply.github.com>
2024-04-18 20:57:39 -0300

    Update carta21.py

--- programacao1/carta21.py
+++ programacao1/carta21.py
@@ -13,10 +13,10 @@ def jogar_jogo():
     print(f'Você ganhou a carta com valor {carta_jogador} sua pontuação atual é {pontos_jogador}')
 # verifica se a soma das cartas é maior que 21
     if pontos_jogador > 21:
-      print("Você ultrapassou 21 pontos, o computador venceu)
+      print("Você ultrapassou 21 pontos, o computador venceu")
       break
 # verifica se a soma das cartas é igual a 21            
-    if pontos_jogador = 21:
+    if pontos_jogador == 21:
       print("21. Você ganhou")     
       break
 # após logica do jogador, se repete para a mão do computador    
@@ -25,9 +25,9 @@ def jogar_jogo():
     print(f'Você ganhou a carta com valor {carta_pc} sua pontuação atual é {pontos_pc}')
 
     if pontos_pc > 21:
-      print("O computador ultrapassou 21 pontos, o jogador venceu)
+      print("O computador ultrapassou 21 pontos, o jogador venceu")
       break
-    if pontos_pc = 21:
+    if pontos_pc == 21:
       print("21. Computador ganhou")     
       break
 # chama função assim que executa o código. Para iniciar o jogo.      
