10e0908dc62dcc9a0adab6c66c072e5ae1605297
DGzzzzz <douglaswinter75@gmail.com>
2024-08-22 15:16:41 -0300

    alterado algoritmo, pois visto que a pesquisa linear percorre todo array, não há nececidade de verificar a esquerda e direita do indece achado

--- EstruturaDados/src/pesquisa/PesquisaLinear.java
+++ EstruturaDados/src/pesquisa/PesquisaLinear.java
@@ -3,44 +3,24 @@ package pesquisa;
 import java.util.ArrayList;
 
 public class PesquisaLinear {
-    public static int pesquisaLinear(ArrayList<Integer> listaAchados,int lista[], int chave) {
+    public static ArrayList<Integer> pesquisaLinear(int lista[], int chave) {
+        ArrayList<Integer> listaAchados = new ArrayList<Integer>();
         for (int i = 0; i < lista.length; i++) {
             if (lista[i] == chave) {
-                if (listaAchados.isEmpty()) {
-                    listaAchados.add(i);
-
-                    int esquerda = i - 1;
-                    while (esquerda >= 0 && lista[esquerda] == chave) {
-                        listaAchados.add(esquerda);
-                        esquerda--;
-                    }
-
-                    int direita = i + 1;
-                    while (direita < lista.length && lista[direita] == chave) {
-                        listaAchados.add(direita);
-                        direita++;
-                    }
-
-                } else {
-                    if (listaAchados.get(listaAchados.size() - 1) != i) {
-                        listaAchados.add(i);
-                    }
-                }
-                return i;
+                listaAchados.add(i);
             }
         }
-        return -1;
+        return listaAchados;
     }
 
     public static void main(String[] args) {
-        int lista[] = { 10, 20, 30, 30, 30, 40, 55, 60, 70, 80, 90 };
-        int chave = 20;
-        ArrayList<Integer> listaAchados = new ArrayList<Integer>();
-        if (pesquisaLinear(listaAchados, lista, chave) != -1) {
+        int lista[] = { 10, 20, 30, 30, 30, 40, 55, 60, 70, 80, 30 };
+        int chave = 30;
+        ArrayList<Integer> listaAchados = pesquisaLinear( lista, chave);
+        if (!listaAchados.isEmpty()) {
             System.out.println("Chave encontrada nas posições: " + listaAchados);
         } else {
             System.out.println("Chave não encontrada");
         }
-        
     }
 }
