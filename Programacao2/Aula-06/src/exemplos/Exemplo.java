29f4155367be67cd03b1a3c7a5d57b8e65c114d8
DGzzzzz <douglaswinter75@gmail.com>
2024-09-03 20:55:34 -0300

    aula-06 - associacao

---
+++ Programacao2/Aula-06/src/exemplos/Exemplo.java
@@ -0,0 +1,22 @@
+package exemplos;
+
+public class Exemplo {
+    public static void main(String[] args) {
+        //Associação da classe endereço na classe pessoa
+
+        Endereco e = new Endereco();
+        e.cidade = "Vale Real";
+        e.rua = "Rua";
+        e.complemento = "123";
+        e.CEP = "378237283";
+        e.numero = 123;
+
+
+        Pessoa p = new Pessoa();
+        p.nome = "Douglas";
+        p.endereco = e;
+
+        p.imprimir();
+        e.imprimirEnd();
+    }
+}
