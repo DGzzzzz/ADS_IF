d57bd91beacf9074b37f3792349c23009acc0c2f
DGzzzzz <douglaswinter@outlook.com>
2024-09-18 08:34:34 -0300

    ordenação merge sort

--- EstruturaDados/atividadesPraticas/atividade2.py
+++ EstruturaDados/atividadesPraticas/atividade2.py
@@ -13,9 +13,34 @@ for linha in linhas:
         nomes.append(nome)
         if qi != 'IQ':
             qis.append(qi)
+
+def ordena_lista(lista):
+    n = len(lista)
+    if n > 1:
+        meio = n // 2
+        listaDaEsquerda = lista[:meio]
+        listaDaDireita = lista[meio:]
+        
+        ordena_lista(listaDaEsquerda)
+        ordena_lista(listaDaDireita)
+        
+        i = j = k = 0
+        while i < len(listaDaEsquerda) and j < len(listaDaDireita):
+            if listaDaEsquerda[i] < listaDaDireita[j]:
+                lista[k] = listaDaEsquerda[i]
+                i += 1
+            else:
+                lista[k] = listaDaDireita[j]
+                j += 1
+            k += 1
             
-nomes.sort()        
-qis.sort()
+        # Copia os elementos restantes
+        lista[k:] = listaDaEsquerda[i:] or listaDaDireita[j:]    
+            
+    return lista
+       
+ordena_lista(qis)
+ordena_lista(nomes)     
 
 homens = []
 mulheres = []
