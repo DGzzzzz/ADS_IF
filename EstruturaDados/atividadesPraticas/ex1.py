a6cdef49f1aedc69e1668ae6d44d0b461fa993b2
DGzzzzz <douglaswinter@outlook.com>
2024-09-18 08:29:21 -0300

    adicionado ordenação nas listas. Utilizado merge sort

--- EstruturaDados/atividadesPraticas/ex1.py
+++ EstruturaDados/atividadesPraticas/ex1.py
@@ -7,6 +7,38 @@ total_golds = []
 total_silvers = []
 total_bronzes = []
 
+def ordena_lista(lista):
+    n = len(lista)
+    if n > 1:
+        meio = n // 2
+        listaDaEsquerda = lista[:meio]
+        listaDaDireita = lista[meio:]
+        
+        ordena_lista(listaDaEsquerda)
+        ordena_lista(listaDaDireita)
+        
+        i = j = k = 0
+        while i < len(listaDaEsquerda) and j < len(listaDaDireita):
+            if listaDaEsquerda[i] < listaDaDireita[j]:
+                lista[k] = listaDaEsquerda[i]
+                i += 1
+            else:
+                lista[k] = listaDaDireita[j]
+                j += 1
+            k += 1
+            
+        # Copia os elementos restantes
+        lista[k:] = listaDaEsquerda[i:] or listaDaDireita[j:]
+        
+    return lista
+
+ordena_lista(country_names)
+ordena_lista(total_medals)
+ordena_lista(total_golds)
+ordena_lista(total_silvers)
+ordena_lista(total_bronzes)
+
+
 for linha in linhas[1:]:
     partes = linha.split(',')
     if(len(partes) > 5):
